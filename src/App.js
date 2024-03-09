f99be6f012fb93514be7db0cf2ba67b6c79679f0
DevSoni2022 <121338813+DevSoni2022@users.noreply.github.com>
2024-03-10 01:13:36 +0530

    desktop

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import TodoList from './components/TodoList';
 import Header from './components/Header/Header';
 import './todo.scss'
 const App = () => {
+  const isMobile = navigator && navigator.userAgentData.mobile;
   return (
     <div className="App">
       <Header/>
-      <TodoList />
+      <TodoList isMobile = {isMobile} />
     </div>
   )
 }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,64 +1,96 @@
 import React, { useState, useEffect } from "react";
-import { PlusIcon, InfoIcon, EditIcon, DeleteIcon } from "./ImgStore";
+import {
+  PlusIcon,
+  InfoIcon,
+  EditIcon,
+  DeleteIcon,
+  Update,
+  isEditClicked,
+  isEditedTrue,
+} from "./ImgStore";
 import NoTask from "./NoTask.js";
-import DeleteModal from './DeleteModal/index.js'
+import DeleteModal from "./DeleteModal/index.js";
 const TodoList = () => {
   const [toDoData, setTodoData] = useState([]);
   const [title, setTitle] = useState("");
   const [discription, setDiscription] = useState("");
-  const [showModal,setShowModal] = useState(false);
-  const [idToDelete,setIdtoDelete] = useState(null)
-  const AddToDo = (id, title, discription) => {
+  const [showModal, setShowModal] = useState(false);
+  const [idToDelete, setIdtoDelete] = useState(null);
+  const [isupdate, setIsUpdate] = useState(false);
+  const [isupdateId, setIsUpdateID] = useState(false);
 
+  const AddToDo = (id, title, discription) => {
     const item = {
       id: id,
       title: title,
       discription: discription,
       isEdit: false,
+      isEditClicked: false,
     };
     setTodoData([...toDoData, item]);
-    setTitle('')
-    setDiscription('')
+    setTitle("");
+    setDiscription("");
   };
-  const editHandle = (e,ele) => {
-    if (!ele.isEdit){
-          e.stopPropagation();
-    debugger
-    let copytoDoData = toDoData;
-    let modifyProperty = (copytoDoData, targetId, newProperty) => {
-      let targetObj = copytoDoData.find((obj) => obj.id === targetId);
-      if (targetObj) {
-        targetObj.isEdit = newProperty;
-      }
-    };
+  const editHandle = (e, ele) => {
+    if (!ele.isEdit) {
+      e.stopPropagation();
+      let copytoDoData = toDoData;
+      let modifyProperty = (copytoDoData, targetId, newProperty) => {
+        let targetObj = copytoDoData.find((obj) => obj.id === targetId);
+        if (targetObj) {
+          targetObj.isEdit = newProperty;
+        }
+      };
 
-   
       modifyProperty(copytoDoData, ele.id, !ele.isEdit);
       copytoDoData = [...copytoDoData];
       console.log(copytoDoData, "#$#@!$#@");
       setTodoData(copytoDoData);
-    } 
-    
-
-   
+    } else if (ele.isEdit && !ele.isEditClicked) {
+      let copytoDoData = toDoData;
+      let modifyProperty = (copytoDoData, targetId, newProperty) => {
+        let targetObj = copytoDoData.find((obj) => obj.id === targetId);
+        if (targetObj) {
+          targetObj.isEditClicked = newProperty;
+        }
+      };
+      modifyProperty(copytoDoData, ele.id, !ele.isEditClicked);
+      copytoDoData = [...copytoDoData];
+      console.log(copytoDoData, "#$#@!$#@");
+      setTitle(ele.title);
+      setTodoData(copytoDoData);
+      setDiscription(ele.discription);
+      setIsUpdate(true);
+      setIsUpdateID(ele.id);
+    }
   };
   const deletItem = (id) => {
-    debugger
-    // let copytoDoData = toDoData;
-    // let newTodo = copytoDoData.filter((ele) => ele.id !== id);
-    // setTodoData(newTodo);
-setIdtoDelete(id)
-    setShowModal(true)
+    setIdtoDelete(id);
+    setShowModal(true);
   };
-  const deleteModal=()=>{
-     let copytoDoData = toDoData;
+  const deleteModal = () => {
+    let copytoDoData = toDoData;
     let newTodo = copytoDoData.filter((ele) => ele.id !== idToDelete);
     setTodoData(newTodo);
-    setShowModal(false)
-  }
-  const noDelete=()=>{
-    setShowModal(false)
-  }
+    setShowModal(false);
+  };
+  const noDelete = () => {
+    setShowModal(false);
+  };
+  const updateList = (id, title, discription) => {
+    let newData = toDoData;
+    let getIndex = newData.findIndex((ele) => ele.id == id);
+    newData[getIndex] = {
+      id: id,
+      title: title,
+      discription: discription,
+      isEdit: false,
+      isEditClicked: false,
+    };
+    newData = [...newData];
+    setTodoData(newData);
+    setIsUpdate(false);
+  };
   return (
     <div className="TodoList">
       <div className="todo-input">
@@ -76,8 +108,18 @@ setIdtoDelete(id)
             onChange={(e) => setDiscription(e.target.value)}
           />
         </div>
-        <div onClick={() => AddToDo(Date.now(), title, discription)}>
-          {<PlusIcon />}
+        <div>
+          {!isupdate ? (
+            <span onClick={() => AddToDo(Date.now(), title, discription)}>
+              {" "}
+              <PlusIcon />
+            </span>
+          ) : (
+            <span onClick={() => updateList(isupdateId, title, discription)}>
+              {" "}
+              <Update />
+            </span>
+          )}
         </div>
       </div>
       {toDoData && toDoData.length > 0 ? (
@@ -89,7 +131,7 @@ setIdtoDelete(id)
                   <div
                     key={ele.id}
                     className="list-item"
-                    onClick={(e) =>(e.stopPropagation(), editHandle(e,ele))}
+                    onClick={(e) => (e.stopPropagation(), editHandle(e, ele))}
                   >
                     <div className="content">
                       <div className="title">{ele.title}</div>
@@ -97,9 +139,13 @@ setIdtoDelete(id)
                     </div>
                     {ele.isEdit ? (
                       <>
-                        <span>
-                          <EditIcon />
-                        </span>{" "}
+                        <span onClick={(e) => editHandle(e, ele)}>
+                          {ele.isEditClicked ? (
+                            <img src={"/edit.svg"} alt="img" />
+                          ) : (
+                            <EditIcon />
+                          )}
+                        </span>
                         <span onClick={() => deletItem(ele.id)}>
                           <DeleteIcon />
                         </span>{" "}
@@ -115,17 +161,17 @@ setIdtoDelete(id)
       ) : (
         <NoTask />
       )}
-      {
-        showModal && <div className='modal-main'>
-        <div className='modal-content'>
-            <span className='modal-text'>Delete this task ?</span>
-            <div className='btn-container'>
-                <button onClick={()=>deleteModal()}>Yes</button>
-                <button onClick={()=>noDelete()}>No</button>
+      {showModal && (
+        <div className="modal-main">
+          <div className="modal-content">
+            <span className="modal-text">Delete this task ?</span>
+            <div className="btn-container">
+              <button onClick={() => deleteModal()}>Yes</button>
+              <button onClick={() => noDelete()}>No</button>
             </div>
+          </div>
         </div>
-    </div>
-      }
+      )}
     </div>
   );
 };
